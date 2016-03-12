bea44aaf17ee1166e2bc73cd30c3bed101066f2f
Pascal Tene <>
2016-03-12 22:47:55 +0000

    Vscan version 0.05 Can now connect to database to check for Vulnerabilities

--- src/vscan/ConnectDB.java
+++ src/vscan/ConnectDB.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * @Author: Pascal Tene
  */
 package vscan;
 
@@ -16,22 +14,21 @@ import java.util.ArrayList;
  * @author Pascal Tene
  */
 public class ConnectDB {
+    int numberOfVulnerabilities = 0;
     
      Connection connection;
 	public ConnectDB() throws Exception	{
 	try {
 	Class.forName("com.mysql.jdbc.Driver").newInstance();
-	System.out.println("Connected To MySQL");
+	System.out.println("Database driver loaded");
 	} catch (ClassNotFoundException e) {
 	System.out.println("Error: Cannot load the driver");
 	}
-
 	}
 
 	public void connectToDB() throws Exception {
 		try {
-			connection = DriverManager.getConnection (
-				"jdbc:mysql://localhost/vscan", "root", "jt.pas");
+			connection = DriverManager.getConnection ("jdbc:mysql://localhost/vscan", "root", "jt.pas");
 			System.out.println("Connected to the database.");
 			} catch (Exception e) {
 				System.out.println("Cannot connect to the database.");
@@ -41,14 +38,14 @@ public class ConnectDB {
         public void execSQL(ArrayList<String> recivedlisOfCodes) {
             ArrayList<String> listOfCodes = new ArrayList<String>();
             listOfCodes = recivedlisOfCodes;
-            System.out.println("Number of vulnerabilities found: "+listOfCodes.size());
+            
             for (String code: listOfCodes){
-               // System.out.println("Curently in execSQL "+code); This can show the elements of the list of codes received for debugging.
-            try {
+               try {
                 Statement stmt = connection.createStatement();
                 ResultSet rs = stmt.executeQuery("Select * from vulnerabilities");
                 while (rs.next()) {
                     if(rs.getString(2).equalsIgnoreCase(code)){
+                    numberOfVulnerabilities+=1;
                     System.out.println("\n");
                     System.out.println(rs.getString(1)+ ":  " + rs.getString(2)+ " " + rs.getString(3)+ " " + rs.getString(4)+ "\n Category: " + rs.getString(5)+ " \n CVSS Base Score: " + rs.getString(6)+ " \n Severity: " + rs.getString(7)
                            + " \n Ask F5 Solution: " + rs.getString(11)+ " \n Solution link: " + rs.getString(10) + " \n Last updated: " + rs.getString(9));
@@ -59,7 +56,7 @@ public class ConnectDB {
             }
             System.out.println();
             }
-            
+             System.out.println("Number of vulnerabilities found "+numberOfVulnerabilities);
            
         }
 
